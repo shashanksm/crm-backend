edaa35d0cc9b494c34f401cea38bcff77ea30754
shashanksm <shashank.mutgi@gmail.com>
2024-06-24 18:04:11 +0530

    new design for RESTful services

--- src/main/java/io/shashanksm/crm/customer/account/controllers/AccountsControllerExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package io.shashanksm.crm.customer.account.controllers;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class AccountsControllerExceptionHandler {
-	
-}
--- src/main/java/io/shashanksm/crm/customer/configurations/LocalDataConfiguration.java
+++ src/main/java/io/shashanksm/crm/customer/configurations/LocalDataConfiguration.java
@@ -1,10 +1,7 @@
-package io.shashanksm.crm.customer.account.configurations;
+package io.shashanksm.crm.customer.configurations;
 
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,11 +12,9 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @PropertySource(value = "classpath:db-config.properties")
-@Profile(value = {"local","dev","test"})
-public class LocalAccountsDataConfiguration {
-	
-	private static final Logger logger = LoggerFactory.getLogger(LocalAccountsDataConfiguration.class);
-	
+@Profile(value = {"local","dev","test","default"})
+public class LocalDataConfiguration {
+		
 	@Value("${db.url}")
 	private String dbUrl;
 	
@@ -37,9 +32,6 @@ public class LocalAccountsDataConfiguration {
 	@Bean
 	DataSource dataSource() {
 		
-		logger.info(dbUrl);
-		logger.info(username);
-		logger.info(password);
 		DriverManagerDataSource d = new DriverManagerDataSource(dbUrl, username, password);
 		return d;
 	}
